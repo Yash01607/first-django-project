459670cb480b08b748d78f5c94314a8a7d40759e
Yash Agrawal <yash.agrawal@beehyv.com>
2023-09-21 19:00:29 +0530

    Udated Allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--k$4up#@i&#joo-lop(t&vv+0s*$n%iriuq6mneoqe8#-x6-2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "yash01.pythonanywhere.com"]
 
 # Application definition
 
